6b8044536b3d84c36fba5e1e3df5b5539b6c6daa
vergilchiou <vergilchiou@gmail.com>
2016-05-16 20:50:48 +0800

    Add files via upload

---
+++ week3_programming assigment.R
@@ -0,0 +1,26 @@
+#創建一個特殊的矩陣
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+}
+#計算反矩陣
+cacheSolve <- function(x, ...) {
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+}
